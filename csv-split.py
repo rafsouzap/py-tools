98ffc43826e8e85a328ffdd36d1a1d08ef773350
Rafael de Paula <rafsouzap@icloud.com>
2024-02-19 22:08:03 -0300

    Update arguments

--- csv-split.py
+++ csv-split.py
@@ -6,8 +6,9 @@ parser = argparse.ArgumentParser(description='Splits a CSV file into smaller chu
 parser.add_argument('-f', '--file', type=str, required=True, help='The CSV file to be split')
 parser.add_argument('-c', '--chunkSize', type=int, default=10000, help='The chunk size for reading the CSV file')
 parser.add_argument('-n', '--columnName', type=str, required=True, help='The name of the column to be extracted from the CSV file')
-parser.add_argument('-e', '--endline', type=str, default='', help='The endline to be used when create a TXT file')
+parser.add_argument('-l', '--endline', type=str, default='', help='The endline to be used when create a TXT file')
 parser.add_argument('-q', '--quotechar', type=str, default='"', help='The quotechar to be used when create a TXT file (default: ")')
+parser.add_argument('-e', '--encoding', type=str, required=False, default='ISO-8859-1', help='The encoding to be used when reading the CSV file (default: ISO-8859-1)')
 
 args = parser.parse_args()
 
@@ -16,7 +17,7 @@ file_counter = 0
 try:
     filename_without_ext = os.path.splitext(args.file)[0]
 
-    for chunk in pd.read_csv(args.file, chunksize=args.chunkSize):
+    for chunk in pd.read_csv(args.file, encoding=args.encoding, chunksize=args.chunkSize):
         selected_column = chunk[args.columnName]
         selected_column = selected_column.apply(lambda x: f'{args.quotechar}{x}{args.quotechar}{args.endline}')
 
